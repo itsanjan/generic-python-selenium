63a458001a94cc61d392e4fd56b83441910afc79
Anjan Kumar <itsanjan2015@gmail.com>
2019-05-27 23:39:56 +0530

    base commit

--- source/base.py
+++ source/base.py
@@ -1,14 +1,14 @@
+from settings import*
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-import os
 
-ENV_CWD = os.getcwd()
-OUTPUT_LOG_SELENIUM = ENV_CWD + '\output' + '\selenium.log'
-print(OUTPUT_LOG_SELENIUM)
-TEST_EXECUTION_BROWSER = 'chrome'
+def set_logging_variable():
+    global OUTPUT_LOG_SELENIUM, TEST_EXECUTION_BROWSER
+    OUTPUT_LOG_SELENIUM = ENV_CWD + '\\output' + '\\selenium.log'
+    TEST_EXECUTION_BROWSER = 'chrome'
 
-print(OUTPUT_LOG_SELENIUM)
+
 def create_selenium_driver(browser='chrome'):
     if browser == 'chrome':
         return webdriver.Chrome(
@@ -26,16 +26,7 @@ def create_selenium_driver(browser='chrome'):
         return webdriver.PhantomJS()
     else:
         msg = 'Selenium driver for browser %s is not available' % browser
-        raise RuntimeError(msg) 
-
-driver = create_selenium_driver(TEST_EXECUTION_BROWSER)
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+        raise RuntimeError(msg)
 
 
+set_logging_variable()
---
+++ source/settings.py
@@ -0,0 +1,5 @@
+import os
+
+ENV_CWD = os.getcwd()
+OUTPUT_LOG_SELENIUM = ENV_CWD + '\\output' + '\\selenium.log'
+TEST_EXECUTION_BROWSER = 'chrome'
